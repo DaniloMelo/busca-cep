ffd6ac11d902a6a1c76207b0ff6e70bb326061c0
Danilo Melo <danilomarquesdemelo@gmail.com>
2023-03-14 17:19:52 -0300

    Form Component created

---
+++ src/Components/Form/index.jsx
@@ -0,0 +1,14 @@
+import './Form.css'
+
+const Form = props => {
+
+    return(
+        <section className='form'>
+            <form>
+                
+            </form>
+        </section>
+    )
+}
+
+export default Form
